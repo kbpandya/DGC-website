8fa2246e3f08ceb3f8d3d5c70e018e6499b65044
Stardust <stardust@local.com>
2026-01-21 02:52:26 -0500

    chore: setup github pages deployment with astro action

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://danvillegastro.com',
+  site: 'https://kbpandya.github.io',
+  base: '/DGC-website',
   image: {
     domains: ['placehold.co'],
   },
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [sitemap()]
 });
